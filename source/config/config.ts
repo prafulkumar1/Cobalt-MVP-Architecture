d62ee04cc7d8bee4e586d2f272072d7d1c6ee017
Praful Kumar Banala <PrafulB@ecssi.local>
2025-01-21 10:26:34 +0530

    added the configuration files

---
+++ source/config/config.ts
@@ -0,0 +1,5 @@
+export const baseURL = ""
+
+export const endpoints = {
+    LOGIN:""
+}
---
+++ source/utlis/api.ts
@@ -0,0 +1,55 @@
+import axios from "axios";
+import { baseURL } from "../config/config";
+ 
+const api = axios.create({
+    baseURL,
+});
+api.interceptors.request.use((request) => {
+  console.log("Starting Request", request.url);
+  return request;
+});
+ 
+api.interceptors.response.use((response) => {
+  console.log("Response:", response.data);
+  return response;
+});
+export default api;
+ 
+//No sonar
+// const response = await api.get(endpoints, {
+//   headers: {
+//     "Content-Type": "application/json",
+//     token: userToken,
+//   },
+// });
+ 
+ 
+// const response = await api.post(endpoints,{},
+//        {
+//          headers: {
+//            "Content-Type": "application/json",
+//            token: userToken,
+//          },
+//        }
+//      );
+ 
+//No sonar
+//  const response = await api.put(endpoints + id, {},
+//    {
+//      headers: {
+//        "Content-Type": "application/json",
+//        token: userToken,
+//      },
+//    }
+//  );
+ 
+//No sonar
+//   const response = await api.delete(
+//     endpoints + id,
+//     {
+//       headers: {
+//         "Content-Type": "application/json",
+//         token: userToken,
+//       },
+//     }
+//   );
