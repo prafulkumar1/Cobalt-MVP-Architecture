0175a98cd1a3e2da6668be65a06544b297db6723
MuskuriAdarsh <adarshm@ecssi.com>
2025-02-18 18:54:07 +0530

    Update ItemModifierUI.js
    itemModifier scroll Animation changes

--- source/views/ItemModifier/ItemModifierUI.js
+++ source/views/ItemModifier/ItemModifierUI.js
@@ -1,200 +1,209 @@
+ 
 import * as UI from '@/components/cobalt/importUI';
-import React from 'react';
-import { Image, Modal } from "react-native";
+import React, { useRef, useEffect, useState } from 'react';
+import { Image, Modal, Animated, LayoutAnimation, UIManager, Platform } from "react-native";
 import { useFormContext } from '@/components/cobalt/event';
 import { styles } from '@/source/styles/ItemModifier';
 import { useItemModifierLogic } from '@/source/controller/itemModifier/ItemModifier';
 import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
 import CbLoader from '@/components/cobalt/cobaltLoader';
-import { TextInput } from 'react-native-gesture-handler';
 import { CbDottedLine } from '@/source/constants/dottedLine';
-
-const pageId = "ItemModifier"
+const pageId = "ItemModifier";
+ 
+// Enable Layout Animation for Android
+if (Platform.OS === "android" && UIManager.setLayoutAnimationEnabledExperimental) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+ 
 const ItemModifier = (props) => {
-   
-    const {  singleItemDetails,modifierData,setFormFieldData,getFormFieldData,cartData,modifiersResponseData,isVisible,setIsVisible } = useFormContext()
-
-    const {ImageUrl,Item_Name,Price,Description,response} = singleItemDetails
+    const { singleItemDetails, modifierData, setFormFieldData, getFormFieldData, cartData, modifiersResponseData, isVisible, setIsVisible,storeSingleItem } = useFormContext();
+    const { ImageUrl, Item_Name, Price, Description, response } = singleItemDetails ;
     const cartItem = cartData?.find((item) => item.Item_ID === singleItemDetails?.Item_ID);
     const quantity = cartItem ? cartItem.quantity : 0;
     let categoryData = typeof modifiersResponseData?.Categories == "string"? JSON.parse(modifiersResponseData?.Categories): modifiersResponseData?.Categories
-    const { handleCloseItemDetails,handleDiscardChanges,loading} = useItemModifierLogic()
-
+    const { handleCloseItemDetails, handleDiscardChanges, loading } = useItemModifierLogic();
+ 
+   
+    const scrollY = useRef(new Animated.Value(0)).current;
+ 
     return (
-      <>
-          <UI.Box style={[styles.mainContainer,styles.itemMainContainer]}>
-            <UI.TouchableOpacity onPress={() => setIsVisible(false)}>
-              <UI.CbImage
-                imageJsx={
-                  <Image
-                    alt="image"
-                    source={{ uri: ImageUrl ? ImageUrl : "" }}
-                    style={styles.itemImage}
-                    resizeMode="cover"
-                  />
-                }
-              />
-            </UI.TouchableOpacity>
-
-            <UI.Box style={styles.modifierContainer}>
-              <UI.Box
+        <>
+ 
+            <Animated.View
                 style={[
-                  styles.itemDetailsContainer,
-                  { marginRight: quantity !== 0 && responsiveWidth(5) },
+                    styles.fixedNameComp,
+                    {
+                        opacity: scrollY.interpolate({
+                            inputRange: [310, 315],
+                            outputRange: [0, 1],
+                            extrapolate: 'clamp',
+                        }),
+                    },
                 ]}
-              >
-                <UI.Box style={{ marginTop: 10 }}>
-                  <UI.Text style={styles.foodItemName}>
-                    {Item_Name ? Item_Name : ""}
-                  </UI.Text>
-                  <UI.Text style={styles.foodItemPrice}>${Price}</UI.Text>
-                </UI.Box>
-
-                <UI.Box
-                  style={[
-                    styles.rightItemContainer,
-                    {
-                      width:
-                        quantity >= 1
-                          ? responsiveWidth(25)
-                          : responsiveWidth(17) ,
-                    },
-                  ]}
-                >
-                  <UI.TouchableOpacity style={styles.favIconBtn}>
-                    {modifiersResponseData?.IsFavorite == 1 ? (
-                      <UI.CbImage
-                        imageJsx={
-                          <Image
-                            source={require("@/assets/images/icons/Fav3x.png")}
-                            style={styles.favIcon}
-                            resizeMode='contain'
-                          />
-                        }
-                      />
-                    ) : (
-                      <UI.CbImage
-                        imageJsx={
-                          <Image
-                            source={require("@/assets/images/icons/Notfav3x.png")}
-                            style={styles.favIcon}
-                            resizeMode='contain'
-                          />
-                        }
-                      />
-                    )}
-                  </UI.TouchableOpacity>
-                  <UI.CbAddToCartButton
-                    mealItemDetails={singleItemDetails}
-                    style={styles.addBtn}
-                  />
-                </UI.Box>
-              </UI.Box>
-
-
-            {
-              Description &&
-              <UI.Box style={styles.foodDiscripContainer}>
-                <CbDottedLine length={50} dotSize={6} dotColor="#0000002B" />
-                <UI.Text style={styles.foodDiscripTxt}>
-                  {Description ? Description : ""}
-                </UI.Text>
-                <CbDottedLine length={50} dotSize={6} dotColor="#0000002B" />
-              </UI.Box>
-            }
-
-              <UI.Box style={styles.modifierSubContainer}>
-                <UI.ScrollView
-                  showsVerticalScrollIndicator={false}
-                  contentContainerStyle={{paddingBottom:responsiveHeight(55)}}
-                  style={[
-                    styles.mainList,
-                    {
-                      marginBottom:
-                        response?.IsModifierAvailable == 0
-                          ? responsiveHeight(10)
-                          : responsiveHeight(30),
-                    },
-                  ]}
-                >
-                  {response?.IsModifierAvailable === 0 && loading ? (
-                    <CbLoader />
-                  ) : (
-                    <UI.Box>
-                      {
-                        categoryData?.length > 0 &&
-                        <>  <UI.Text style={styles.modifierTxt}>Modifiers</UI.Text>
-                          <UI.CbAccordionlist
-                            componentData={modifierData}
-                            screenName="Modifiers"
-                            props={props}
-                          /></>
-                      }
-                    </UI.Box>
-                  )}
-                  <UI.Box>
-                    <UI.Text style={styles.allergyInfoTxt}>
-                      Comment/Allergy Info
+            >
+                <UI.Box style={{ marginTop: 10, }}>
+                    <UI.Text style={styles.foodItemName}>
+                        {Item_Name || ""}
                     </UI.Text>
-                    <UI.cbForm
-                      formId={pageId}
-                      setFormFieldData={setFormFieldData}
-                      getFormFieldData={getFormFieldData}
-                    >
-                      <UI.cbInput id="Comments" style={styles.commentsBox} />
-                    </UI.cbForm>
-                  </UI.Box>
-                </UI.ScrollView>
-              </UI.Box>
-            </UI.Box>
-          </UI.Box>
-
-        <Modal
-          visible={isVisible}
-          transparent
-          animationType="fade"
-          onRequestClose={() => setIsVisible(false)}
-        >
-          <UI.View
-            style={styles.modalContainer}
-            onPress={() => setIsVisible(false)}
-          />
-
-          <UI.Box style={styles.confirmMdl}>
-            <UI.Box style={styles.innerModal}>
-              <UI.Box style={styles.innerModalMsgContainer}>
-                <Image
-                  source={require("@/assets/images/icons/dining3x.png")}
-                  style={styles.diningIcon}
-                />
-                <UI.Text style={styles.innerModalAlertTxt}>
-                  Are you sure you want to discard your changes?
-                </UI.Text>
-
-                <UI.Box style={styles.discardBtn}>
-                  <UI.TouchableOpacity
-                    onPress={() => setIsVisible(false)}
-                    style={styles.modalNoYesBtn}
-                  >
-                    <UI.Text style={styles.modalNoYesBtnTxt}>No</UI.Text>
-                  </UI.TouchableOpacity>
-
-                  <UI.TouchableOpacity
-                    onPress={handleDiscardChanges}
-                    style={styles.modalNoYesBtn}
-                  >
-                    <UI.Text style={styles.modalNoYesBtnTxt}>Yes</UI.Text>
-                  </UI.TouchableOpacity>
+                    <UI.Text style={styles.foodItemPrice}>
+                        ${Price}
+                    </UI.Text>
                 </UI.Box>
-              </UI.Box>
-            </UI.Box>
-          </UI.Box>
-        </Modal>
-      </>
+                <UI.Box style={[styles.rightItemContainer, { width: quantity >= 1 ?responsiveWidth(25): responsiveWidth(17)   }]}>
+                                    <UI.TouchableOpacity style={styles.favIconBtn}>
+                                        {
+                                            modifiersResponseData?.IsFavorite == 1 ?
+                                                <UI.CbImage imageJsx={<Image source={require("@/assets/images/icons/Fav3x.png")} style={styles.favIcon} />} />
+                                                :
+                                                <UI.CbImage imageJsx={<Image source={require("@/assets/images/icons/Notfav3x.png")} style={styles.favIcon} />} />
+                                        }
+                                    </UI.TouchableOpacity>
+                                    <UI.CbAddToCartButton mealItemDetails={singleItemDetails} style={styles.addBtn} />
+                                </UI.Box>
+            </Animated.View>
+ 
+ 
+            <Animated.ScrollView
+                contentContainerStyle={{borderTopLeftRadius:35,borderTopRightRadius:35}}
+                onScroll={Animated.event(
+                    [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+                    { useNativeDriver: false }
+                )}
+                scrollEventThrottle={10}>
+                <UI.Box style={styles.itemMainContainer}>
+                    <UI.Box style={styles.mainContainer}>
+ 
+ 
+ 
+                        <UI.Box >
+                            <UI.TouchableOpacity onPress={() => setIsVisible(false)}>
+                                <UI.CbImage
+                                    imageJsx={
+                                        <Image
+                                            alt='image'
+                                            source={{ uri: ImageUrl || "" }}
+                                            style={styles.itemImage}
+                                            resizeMode='cover'
+                                        />
+                                    }
+                                />
+                            </UI.TouchableOpacity>
+                        </UI.Box>
+ 
+ 
+                        <Modal
+                            visible={isVisible}
+                            transparent
+                            animationType="fade"
+                            onRequestClose={() => setIsVisible(false)}
+                        >
+                            <UI.View style={styles.modalContainer} onPress={() => setIsVisible(false)} />
+                            <UI.Box style={styles.confirmMdl}>
+                                <UI.Box style={styles.innerModal}>
+                                    <UI.Box style={styles.innerModalMsgContainer}>
+                                        <Image source={require("@/assets/images/icons/dining3x.png")} style={styles.diningIcon} />
+                                        <UI.Text style={styles.innerModalAlertTxt}>
+                                            Are you sure you want to discard your changes?
+                                        </UI.Text>
+ 
+                                        <UI.Box style={styles.discardBtn}>
+                                            <UI.TouchableOpacity
+                                                onPress={() => setIsVisible(false)}
+                                                style={styles.modalNoYesBtn}
+                                            >
+                                                <UI.Text style={styles.modalNoYesBtnTxt}>No</UI.Text>
+                                            </UI.TouchableOpacity>
+ 
+                                            <UI.TouchableOpacity
+                                                onPress={handleDiscardChanges}
+                                                style={styles.modalNoYesBtn}
+                                            >
+                                                <UI.Text style={styles.modalNoYesBtnTxt}>Yes</UI.Text>
+                                            </UI.TouchableOpacity>
+                                        </UI.Box>
+                                    </UI.Box>
+                                </UI.Box>
+                            </UI.Box>
+                        </Modal>
+ 
+ 
+                        <UI.Box style={[styles.modifierContainer, { marginTop: 0 }]}>
+                            <UI.Box style={[styles.itemDetailsContainer, { marginRight: quantity !== 0 && responsiveWidth(5) }]}>
+                                <UI.Box style={{ marginTop: 10 }}>
+                                    <UI.Text style={styles.foodItemName}>
+                                    {Item_Name?Item_Name:""}
+                                    </UI.Text>
+                                    <UI.Text style={styles.foodItemPrice}>
+                                        ${Price}
+                                    </UI.Text>
+                                </UI.Box>
+ 
+                                <UI.Box style={[styles.rightItemContainer, { width: quantity >= 1 ?responsiveWidth(25): responsiveWidth(17) }]}>
+                                    <UI.TouchableOpacity style={styles.favIconBtn}>
+                                        {
+                                            modifiersResponseData?.IsFavorite == 1 ?
+                                                <UI.CbImage imageJsx={<Image source={require("@/assets/images/icons/Fav3x.png")} style={styles.favIcon} />} />
+                                                :
+                                                <UI.CbImage imageJsx={<Image source={require("@/assets/images/icons/Notfav3x.png")} style={styles.favIcon} />} />
+                                        }
+                                    </UI.TouchableOpacity>
+                                    <UI.CbAddToCartButton mealItemDetails={singleItemDetails} style={styles.addBtn} />
+                                </UI.Box>
+                            </UI.Box>
+ 
+
+                            {
+                                Description &&
+                                <UI.Box style={styles.foodDiscripContainer}>
+                                    <CbDottedLine length={50} dotSize={6} dotColor="#0000002B" />
+                                    <UI.Text style={styles.foodDiscripTxt}>
+                                        {Description ? Description : ""}
+                                    </UI.Text>
+                                    <CbDottedLine length={50} dotSize={6} dotColor="#0000002B" />
+                                </UI.Box>
+                            }
+ 
+                            <UI.Box style={styles.modifierSubContainer}>
+                                <Animated.ScrollView
+                                    showsVerticalScrollIndicator={false}
+                                    style={[styles.mainList, { marginBottom: response?.IsModifierAvailable == 0 ? responsiveHeight(10) : responsiveHeight(30) }]}
+                                    onScroll={Animated.event(
+                                        [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+                                        { useNativeDriver: true }
+                                    )}
+                                    // onScrollEndDrag={handleScrollEnd}
+                                    scrollEventThrottle={16}
+                                >
+                                    {response?.IsModifierAvailable === 0 && loading ? (
+                                        <CbLoader />
+                                    ) : (
+                                        <UI.Box>
+                                        {
+                                          categoryData?.length > 0 &&
+                                          <>  <UI.Text style={styles.modifierTxt}>Modifiers</UI.Text>
+                                            <UI.CbAccordionlist
+                                              componentData={modifierData}
+                                              screenName="Modifiers"
+                                              props={props}
+                                            /></>
+                                        }
+                                      </UI.Box>
+                                    )}
+ 
+                                    <UI.Box>
+                                        <UI.Text style={styles.allergyInfoTxt}>Comment/Allergy Info</UI.Text>
+                                        <UI.cbForm formId={pageId} setFormFieldData={setFormFieldData} getFormFieldData={getFormFieldData}>
+                                            <UI.cbInput id="Comments" style={styles.commentsBox} />
+                                        </UI.cbForm>
+                                    </UI.Box>
+                                </Animated.ScrollView>
+                            </UI.Box>
+                        </UI.Box>
+                    </UI.Box>
+                </UI.Box>
+            </Animated.ScrollView>
+        </>
     );
-    
 };
-
+ 
 export default ItemModifier;
-
-
