7fb6e595a521e12d6978908db3e7373f827541a2
MuskuriAdarsh <adarshm@ecssi.com>
2025-03-13 12:35:36 +0530

    pending order styles

--- source/views/Recentorders/recentOrderUi.js
+++ source/views/Recentorders/recentOrderUi.js
@@ -26,157 +26,137 @@ function RenderingPendingOrders(props) {
   
   return (
 <Accordion
-  style={{
-    paddingHorizontal: horizontalScale(10),
-    width: "100%", 
-    maxHeight: "100%",
-    borderRadius: moderateScale(8),
-    backgroundColor: "#ffffff",
-    shadowColor: "#00000029",
-    shadowOffset: { width: 4, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 10,
-    elevation: 8,
-    padding: moderateScale(10),
-    alignSelf: "center"
-  }}
+  style={styles.pendingOrderAccordion}
 >
   {pendingOrders?.map((Order, index) => (
     <AccordionItem key={index} value={`item-${index}`}>
       <AccordionHeader >
-        <UI.Box style={{ flexDirection: "row", alignItems: "center", paddingVertical:20,justifyContent:"space-between" }}>
-            <UI.CbImage  imageJsx={<Image source={require("@/assets/images/icons/Pendingorder.png")}    style={{ width: 28, height: 28, left:6}}  /> }/>
-            <UI.Text   style={{ fontSize: 18, fontStyle: "italic", fontFamily: "SourceSansPro_Bold",fontWeight: "700",  marginLeft: 10,   left:10  }}>
-               Ordered Status
-            </UI.Text>
-            <UI.Text   style={{ marginLeft: "auto",  color: "#FF6F00", fontSize: 16,fontFamily: "SourceSansPro_Bold", fontWeight: "700",  right:20 }}>   
-              {Order.orderstatus}
-            </UI.Text>
+        <UI.Box style={styles.accordionHeaderBox}>
+          <UI.CbImage imageJsx={<Image source={require("@/assets/images/icons/Pendingorder.png")} style={styles.pendingOrderImg} />} />
+          <UI.Text style={styles.orderStaticStatusTxt}>
+            Ordered Status
+          </UI.Text>
+          <UI.Text style={styles.orderStatusTxt}>
+            {Order.orderstatus}
+          </UI.Text>
         </UI.Box>
         <AccordionTrigger className="focus:web:rounded-lg">
-        <UI.Box  style={{ flexDirection: "row", justifyContent: "space-between",  alignItems: "center",  }}>
+          <UI.Box style={styles.accordionRriggerBox}>
             <UI.Box>
-              <UI.Text   style={{ fontSize:11, color: "#4F4F4F", fontFamily: "SourceSansPro_Bold",  }} >
-                  Pickup Time
-                </UI.Text>
-                <UI.Text  style={{  fontSize: 16, fontFamily: "SourceSansPro_Bold", color:"#2A4E7D"  }} >
-                  {Order.pickuptime}
-                </UI.Text>
+              <UI.Text style={styles.pickUpTimeAndLocationStaticTxt} >
+                Pickup Time
+              </UI.Text>
+              <UI.Text style={styles.pickUpTimeAndLocation} >
+                {Order.pickuptime}
+              </UI.Text>
             </UI.Box>
-            <UI.Box style={{ paddingHorizontal:8,   }}>
-                <UI.Text style={{ fontSize: 11,  color: "#4F4F4F", fontFamily: "SourceSansPro_Bold" }}>
-                    Pickup Place
-                  </UI.Text>
-                <UI.Text style={{ fontSize: 16, fontFamily: "SourceSansPro_Bold",color:"#2A4E7D" }}>
+            <UI.Box style={{ paddingHorizontal: 8, }}>
+              <UI.Text style={styles.pickUpTimeAndLocationStaticTxt}>
+                Pickup Place
+              </UI.Text>
+              <UI.Text style={styles.pickUpTimeAndLocation}>
                 {Order.pickuplocation}
-                </UI.Text>
-              </UI.Box>
-            <UI.Box style={{ paddingHorizontal:60 }}>
-                <UI.Text   style={{fontSize: 14, color: "#4F4F4F", fontFamily: "SourceSansPro_BoldItalic" }} >
-                  Order Id #: {Order.OrderID}
-                </UI.Text>
-                <UI.Text style={{ fontSize: 14,  color: "#4F4F4F", fontFamily: "SourceSansPro_BoldItalic" }} >
-                  Date: {Order.Ordereddate}
-                </UI.Text>
-            </UI.Box> 
+              </UI.Text>
+            </UI.Box>
+            <UI.Box style={{ paddingHorizontal: 60 }}>
+              <UI.Text style={styles.orderIdAndDateStaticTxt} >
+                Order Id #: {Order.OrderID}
+              </UI.Text>
+              <UI.Text style={styles.orderIdAndDateStaticTxt} >
+                Date: {Order.Ordereddate}
+              </UI.Text>
+            </UI.Box>
             <>
-            {({ isExpanded }) => {
-            return (
-              <>
-                {isExpanded ? (
-                  <AccordionIcon as={ChevronRightIcon} className="mr-3" style={{width:20,height:20,}} />
-                ) : (
-                  <AccordionIcon as={ChevronDownIcon} className="mr-3" style={{width:20,height:20,}}/>
-                )}
-              </>
-            )
-          }}
+              {({ isExpanded }) => {
+                return (
+                  <>
+                    {isExpanded ? (
+                      <AccordionIcon as={ChevronRightIcon} className="mr-3" style={{ width: 20, height: 20, }} />
+                    ) : (
+                      <AccordionIcon as={ChevronDownIcon} className="mr-3" style={{ width: 20, height: 20, }} />
+                    )}
+                  </>
+                )
+              }}
             </>
-         </UI.Box>           
+          </UI.Box>           
         </AccordionTrigger>
       </AccordionHeader>
       <AccordionContent >
-        <UI.Box style={{ padding: 12, backgroundColor: "#fff" }}>
-         <UI.Box>
-                <UI.Text style={{ fontSize: 14, fontFamily: "SourceSansPro_Bold",right:18,bottom:10}}>
-                  Order Summary
+        <UI.Box style={styles.accordionContentBox}>
+          <UI.Box>
+            <UI.Text style={styles.orderSummaryStaticTxt}>
+              Order Summary
+            </UI.Text>
+            <UI.Box style={styles.accordionContentBox2}>
+              <UI.Text style={[styles.timeQtyAmtStaticTxt, {right: 18 }]} >
+                Items
+              </UI.Text>
+              <UI.Box style={{ flexDirection: "row", alignItems: "center" }}>
+                <UI.Text style={[styles.timeQtyAmtStaticTxt,{ marginRight: 16 }]}>
+                  Qty
                 </UI.Text>
-                <UI.Box style={{ flexDirection: "row",justifyContent: "space-between",alignItems: "center",marginBottom: 8,}}>
-                    <UI.Text style={{ fontSize: 12, color: "#5773A2",fontFamily: "SourceSansPro_Bold", fontStyle: "italic",right:18 }} >
-                      Items
-                    </UI.Text>
-                    <UI.Box style={{ flexDirection: "row", alignItems: "center" }}>
-                      <UI.Text   style={{ fontSize: 12, color: "#5773A2",fontFamily: "SourceSansPro_Bold", fontStyle: "italic",  marginRight: 16 }}>
-                        Qty
-                      </UI.Text>
-                    <UI.Text style={{ fontSize: 12, color: "#5773A2",fontFamily: "SourceSansPro_Bold", fontStyle: "italic"}}>
-                      Amount
-                    </UI.Text>
-                </UI.Box>
-          </UI.Box>       
-        </UI.Box>
+                <UI.Text style={styles.timeQtyAmtStaticTxt}>
+                  Amount
+                </UI.Text>
+              </UI.Box>
+            </UI.Box>
+          </UI.Box>
 
           <UI.Box style={{ marginBottom: 12 }}>
-          {pendingOrders
-    .filter((order) => order.OrderID === Order.OrderID) // Ensure we are grouping items by order
-    .map((order, index) => (  <React.Fragment key={index}>
-    <UI.Box style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-      <UI.Text style={{ fontSize: 12, fontStyle: "italic", fontWeight: "700", right: 18 }}>
-        {index + 1}. {Order.Item_Name}
-      </UI.Text>
-      <UI.Box style={{ flexDirection: "row", alignItems: "center" }}>
-        <UI.Text style={{ fontSize: 12, fontFamily: "SourceSansPro_Bold", fontStyle: "italic", marginRight: 16 }}>
-          1 {/* Since there is no Quantity field in response, default to 1 */}
-        </UI.Text>
-        <UI.Text style={{ fontSize: 12, fontFamily: "SourceSansPro_Bold", fontStyle: "italic" }}>
-          ${Order.Price.toFixed(2)}
-        </UI.Text>
-      </UI.Box>
-    </UI.Box>
-    <UI.Box>
-      {Order?.Modifiers?.length > 0 && Order.Modifiers.map((Modifier, modIndex) => (
-        <React.Fragment key={modIndex}>
-          <UI.Text style={{ fontSize: 11, color: "#5773A2", fontFamily: "SourceSansPro_Bold", fontStyle: "italic" }}>
-            {Modifier.Item_Name}
-          </UI.Text>
-        </React.Fragment>
-      ))}
-    </UI.Box>
-    {Order.Comments && Order.Comments !== "No Comments added" && (
-      <UI.Box style={{ flexDirection: "row", alignItems: "center", marginTop: 5 }}>
-        <UI.CbImage imageJsx={<Image source={require('@/assets/images/icons/ROComment.png')} style={{ width: horizontalScale(15), height: verticalScale(15), marginRight: 5, top: 1 }} />} />
-        <UI.Text
-          style={{
-            fontSize: 12,
-            fontFamily: "SourceSansPro_Bold",
-            fontStyle: "italic",
-            flexShrink: 1,
-            flexWrap: "wrap",
-            maxWidth: "90%"
-          }}
-        >
-          {Order.Comments}
-        </UI.Text>
-      </UI.Box>
-    )}
-  </React.Fragment>
-))}
+            {pendingOrders
+              .filter((order) => order.OrderID === Order.OrderID) // Ensure we are grouping items by order
+              .map((order, index) => (<React.Fragment key={index}>
+                <UI.Box style={styles.accordionRriggerBox}>
+                  <UI.Text style={styles.itemName}>
+                    {index + 1}. {Order.Item_Name}
+                  </UI.Text>
+                  <UI.Box style={{ flexDirection: "row", alignItems: "center" }}>
+                    <UI.Text style={[styles.qtnAnd$,{ marginRight: 16 }]}>
+                      1 {/* Since there is no Quantity field in response, default to 1 */}
+                    </UI.Text>
+                    <UI.Text style={styles.qtnAnd$}>
+                      ${Order.Price.toFixed(2)}
+                    </UI.Text>
+                  </UI.Box>
+                </UI.Box>
+                <UI.Box>
+                  {Order?.Modifiers?.length > 0 && Order.Modifiers.map((Modifier, modIndex) => (
+                    <React.Fragment key={modIndex}>
+                      <UI.Text style={styles.itemModifierName}>
+                        {Modifier.Item_Name}
+                      </UI.Text>
+                    </React.Fragment>
+                  ))}
+                </UI.Box>
+                {Order.Comments && Order.Comments !== "No Comments added" && (
+                  <UI.Box style={{ flexDirection: "row", alignItems: "center", marginTop: 5 }}>
+                    <UI.CbImage imageJsx={<Image source={require('@/assets/images/icons/ROComment.png')} style={styles.itemCommentsImg} />} />
+                    <UI.Text
+                      style={styles.itemCommentsTxt}
+                    >
+                      {Order.Comments}
+                    </UI.Text>
+                  </UI.Box>
+                )}
+              </React.Fragment>
+              ))}
 
           </UI.Box>
-            <UI.Box style={{ borderTopWidth: 1, borderTopColor: "#eee", marginTop: 12, paddingTop: 12,alignSelf: "flex-end",alignItems: "flex-end",  }}>
-               {Order.SubTotal && <UI.Text style={{ fontWeight: "700", fontFamily: "SourceSansPro_Bold" }}>
-                  Sub Total: ${Order.SubTotal.toFixed(2)}
-                </UI.Text>}
-                {Order.ServiceCharge && <UI.Text>10% Service Charge: ${Order.ServiceCharge.toFixed(2)}</UI.Text>}
-               {Order.FoodTax && <UI.Text>Food Tax: ${Order.FoodTax.toFixed(2)}</UI.Text> }
-                {Order.StateTax && <UI.Text>State Tax: ${Order.StateTax.toFixed(2)}</UI.Text> }
-            </UI.Box>
-            <UI.Box style={{ borderTopWidth: 1, borderTopColor: "#eee", marginTop: 12, paddingTop: 12,alignSelf: "flex-end",alignItems: "flex-end",  }}>
-            {Order.Total &&  <UI.Text style={{ fontWeight: "700", marginTop: 8 }}>Total: ${Order.Total.toFixed(2)}</UI.Text> }
-                {Order.Tip &&   <UI.Text>Tip: ${Order.Tip.toFixed(2)}</UI.Text> }
-                {Order.GrandTotal &&    <UI.Text>Grand Total: ${Order.GrandTotal.toFixed(2)}</UI.Text> }
-            </UI.Box>
-        
+          <UI.Box style={styles.finalAccordionBoxes}>
+            {Order.SubTotal && <UI.Text style={{ fontWeight: "700", fontFamily: "SourceSansPro_Bold" }}>
+              Sub Total: ${Order.SubTotal.toFixed(2)}
+            </UI.Text>}
+            {Order.ServiceCharge && <UI.Text>10% Service Charge: ${Order.ServiceCharge.toFixed(2)}</UI.Text>}
+            {Order.FoodTax && <UI.Text>Food Tax: ${Order.FoodTax.toFixed(2)}</UI.Text>}
+            {Order.StateTax && <UI.Text>State Tax: ${Order.StateTax.toFixed(2)}</UI.Text>}
+          </UI.Box>
+          <UI.Box style={styles.finalAccordionBoxes}>
+            {Order.Total && <UI.Text style={{ fontWeight: "700", marginTop: 8 }}>Total: ${Order.Total.toFixed(2)}</UI.Text>}
+            {Order.Tip && <UI.Text>Tip: ${Order.Tip.toFixed(2)}</UI.Text>}
+            {Order.GrandTotal && <UI.Text>Grand Total: ${Order.GrandTotal.toFixed(2)}</UI.Text>}
+          </UI.Box>
+
         </UI.Box>
       </AccordionContent>
     </AccordionItem>
